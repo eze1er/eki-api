e7416ccd1bfe2268c24d52388943db6c2634f14a
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-10-21 04:49:25 -0400

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const request = require("request-promise");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
