7c0f0d7c585994614ac22a9cd17a6611cb7beabe
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-10-22 03:40:17 -0400

    before deployment

---
+++ README.md
@@ -0,0 +1,24 @@
+## API 
+## Build Deploy Seel and startup
+
+
+* PREMIER POINT
+
+NODE JS a installer si u n'as n'a pas
+
+*npm init -y
+*modifier le .json . "Start": "node index.js"
+
+* npm install express request-promise nodemon
+
+* ajoute dans scripts de .json
+"dev": "nodemon index.js"
+
+* creer index.js
+
+* npm install request
+
+* scraperapi.com
+
+* copy API Key 
+* put it in index.js : apikey end baseUrl;
--- index.js
+++ index.js
@@ -4,10 +4,81 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// const apiKey = 'a79075d062f5c2b26776c6ded816ba73';
+// const generateScraperUrl(apiKey) = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+const generateScraperUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Eki-API.");
+});
+
+// GET Product Details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// GET Product Review
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// GET Product Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// GET Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 app.listen(PORT, () => console.log(`eki-API server running on port ${PORT}`));
